298702dd6ee15314e52b9cb73448d4dfe03b98e9
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-02-11 16:50:32 +0100

    Adding structures using DOM

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,11 +1,32 @@
+const createNav = ()=>{
+    const nav= document.createElement('nav');
+
+    const homeButton = document.createElement('button');
+    homeButton.classList.add('nav-button');
+    homeButton.textContent = 'Home';
+
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('nav-button');
+    menuButton.textContent = 'Menu';
+
+    const contactButton = document.createElement('button');
+    contactButton.classList.add('nav-button');
+    contactButton.textContent = 'Contact';
+    
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
+
+    return nav;
+}
 const createHeader = () => {
  const header = document.createElement('header');
- header.classList.add(header);
+ header.classList.add('header');
 
  const restaurantName = document.createElement('h1');
  restaurantName.classList.add('restaurant-page');
  restaurantName.textContent = 'African Delicacy';
-
+ 
  header.appendChild(restaurantName);
  header.appendChild(createNav());
 
