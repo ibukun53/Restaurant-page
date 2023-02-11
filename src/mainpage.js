b6ca0cdc2f7f7899c45bb98862a315493af04fdd
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-02-11 17:22:09 +0100

    Add ing main and footer

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,3 +1,4 @@
+// creating nav function element
 const createNav = ()=>{
     const nav= document.createElement('nav');
 
@@ -19,6 +20,8 @@ const createNav = ()=>{
 
     return nav;
 }
+
+//creating header function
 const createHeader = () => {
  const header = document.createElement('header');
  header.classList.add('header');
@@ -33,9 +36,50 @@ const createHeader = () => {
  return header;
 }
 
+// creating main body function
+const createMain = () =>{
+    const main = document.createElement('main');
+    main.classList.add('main');
+    return main
+}
+
+
+// creating footer function  element
+const createFooter = ()=>{
+    const footer = document.createElement('footet');
+    footer.classList.add('footer');
+
+    const copyright = document.createElement('p');
+    copyright.textContent=`Copyright © ${new Date().getFullYear()} debbyblessing`
+
+    const githubLink = document.createElement('a');
+    githubLink.href = 'https://github.com/ibukun53/'
+
+    const  githubIcon = document.createElement('i');
+    githubIcon.classList.add('fab');
+    githubIcon.classList.add('fa-github');
+
+    const linkedinLink = document.createElement('a');
+    linkedinLink.href = 'https://www.linkedin.com/in/ibukun53/'
+
+    const linkedinIcon = document.createElement('i');
+    linkedinIcon .classList.add('fab');
+    linkedinIcon .classList.add('fa-linkedin');
+    
+    githubLink.appendChild(githubIcon);
+    linkedinLink.appendChild(linkedinIcon);
+    footer.appendChild(copyright);
+    footer.appendChild(githubLink);
+    footer.appendChild(linkedinLink);
+
+    return footer;
+}
 const StartUpPage = () => {
     const content = document.getElementById("content");
+    
     content.appendChild(createHeader());
+    content.appendChild(createMain());
+    content.appendChild(createFooter());
 }
 
 export default StartUpPage;
